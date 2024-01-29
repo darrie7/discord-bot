4934fe672383124218aea1cd43ecec0926a48b13
darrie7 <darnell.r.kluit@gmail.com>
2024-01-29 16:20:04 +0100

    Update jwt.py

--- cogs/jwt.py
+++ cogs/jwt.py
@@ -193,7 +193,7 @@ class justwatchCog(commands.Cog):
         await inter.response.defer(with_message=True, ephemeral=False)
         r = await to_thread(requests.get, url=self.global_var.url, headers={'content-type': "application/json",'x-apikey': self.global_var.api_key,'cache-control': "no-cache"})
         main = r.json()
-        main_entry = [item for item in main if title.lower() in item["title"].lower()][0]
+        main_entry = next(item for item in main if title.lower() in item["title"].lower())
         await self.bot.get_channel(793878235066400809).send(f"""```{main_entry}```""")
         await to_thread(requests.delete, f"{self.global_var.url}/{main_entry.get('_id')}", headers={'content-type': "application/json",'x-apikey': self.global_var.api_key,'cache-control': "no-cache"})
         self.bot._db3.remove(self.bot._query.title.lower() == title.lower())
