9f5dbb4c2f81bd3593dad962acaa2b4dcbda7c72
darrie7 <darnell.r.kluit@gmail.com>
2024-02-03 19:54:09 +0100

    Update flightscanner.py

--- cogs/flightscanner.py
+++ cogs/flightscanner.py
@@ -50,16 +50,16 @@ class flightcog(commands.Cog):
         url = "https://data.opendatasoft.com/api/explore/v2.1/catalog/datasets/airports-code@public/records?select=column_1%2Ccity_name%2Ccountry_name&order_by=country_name&limit=100&where="
         ## cities = 'or'.join([f"city_name=%22{c}%22%20or%20airport_name%20LIKE%20%22{c}%22" for c in depcity.split(" ")])
         if depcountry:
-            res = await to_thread(requests.get, f"{url}{'or'.join([f'country_name=%22{c}%22' for c in depcountry.split()])}")
+            res = await to_thread(requests.get, f"{url}{'%20or%20'.join([f'country_name=%22{c}%22' for c in depcountry.split()])}")
             self.depcity = [f"{x.get('column_1')}.AIRPORT" for x in res.json().get("results")]
         else:
-            res = await to_thread(requests.get, f"{url}{'or'.join([f'city_name=%22{c}%22%20or%20airport_name%20LIKE%20%22{c}%22' for c in depcity.split()])}")
+            res = await to_thread(requests.get, f"{url}{'%20or%20'.join([f'city_name=%22{c}%22%20or%20airport_name%20LIKE%20%22{c}%22' for c in depcity.split()])}")
             self.depcity = [f"{x.get('column_1')}.AIRPORT" for x in res.json().get("results")]
         if arrcountry:
-            res = await to_thread(requests.get, f"{url}{'or'.join([f'country_name=%22{c}%22' for c in arrcountry.split()])}")
+            res = await to_thread(requests.get, f"{url}{'%20or%20'.join([f'country_name=%22{c}%22' for c in arrcountry.split()])}")
             self.arrcity = [f"{x.get('column_1')}.AIRPORT" for x in res.json().get("results")]
         else:
-            res = await to_thread(requests.get, f"{url}{'or'.join([f'city_name=%22{c}%22%20or%20airport_name%20LIKE%20%22{c}%22' for c in arrcity.split()])}")
+            res = await to_thread(requests.get, f"{url}{'%20or%20'.join([f'city_name=%22{c}%22%20or%20airport_name%20LIKE%20%22{c}%22' for c in arrcity.split()])}")
             self.arrcity = [f"{x.get('column_1')}.AIRPORT" for x in res.json().get("results")]
         self.vacmin = vacmin
         self.vacmax = vacmax
