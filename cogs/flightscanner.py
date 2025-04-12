48a0aa41283b8f51239f5feff2ec55e3632bda3b
darrie7 <darnell.r.kluit@gmail.com>
2025-04-12 02:24:27 +0200

    Update flightscanner.py

--- cogs/flightscanner.py
+++ cogs/flightscanner.py
@@ -6,6 +6,7 @@ from asyncio import run, gather, to_thread, sleep
 from disnake.ext import commands, tasks
 import disnake
 from cryptography.fernet import Fernet
+from fake_useragent import UserAgent
 
 baseurl = "https://flights.booking.com/flights/X/"
 
@@ -18,6 +19,16 @@ async def generate_date_range(vacation_range: tuple[datetime], vacation_length: 
         current_date += timedelta(days=1)
 
     return date_range
+
+async def split_into_chunks_comp(data_list, chunk_size=3):
+  """Splits a list into sublists of at most chunk_size."""
+  if not isinstance(data_list, list):
+      raise TypeError("Input must be a list.")
+  if not isinstance(chunk_size, int) or chunk_size <= 0:
+      raise ValueError("chunk_size must be a positive integer.")
+
+  return [data_list[i : i + chunk_size] for i in range(0, len(data_list), chunk_size)]
+
 
 
 class flightcog(commands.Cog):
@@ -65,18 +76,22 @@ class flightcog(commands.Cog):
         depcountry: departure countries (Netherlands,Belgium)
         arrcountry: arrival countries (Netherlands,Belgium)
         """
+        ua = UserAgent()
+        headers = {
+            "User-Agent": ua.random
+        }
         url = "https://data.opendatasoft.com/api/explore/v2.1/catalog/datasets/airports-code@public/records?select=column_1%2Ccity_name%2Ccountry_name&order_by=country_name&limit=100&where="
         if depcountry:
-            res = await to_thread(requests.get, f"{url}{'%20or%20'.join([f'country_name=%22{c.title()}%22' for c in depcountry.split(',')])}")
+            res = await to_thread(requests.get, f"{url}{'%20or%20'.join([f'country_name=%22{c.title()}%22' for c in depcountry.split(',')])}", headers = headers)
             self.depcity = [f"{x.get('column_1')}.AIRPORT" for x in res.json().get("results")]
         elif depcity:
-            res = await to_thread(requests.get, f"{url}{'%20or%20'.join([f'city_name=%22{c.title()}%22%20or%20airport_name%20LIKE%20%22{c}%22' for c in depcity.split(',')])}")
+            res = await to_thread(requests.get, f"{url}{'%20or%20'.join([f'city_name=%22{c.title()}%22%20or%20airport_name%20LIKE%20%22{c}%22' for c in depcity.split(',')])}", headers = headers)
             self.depcity = [f"{x.get('column_1')}.AIRPORT" for x in res.json().get("results")]
         if arrcountry:
-            res = await to_thread(requests.get, f"{url}{'%20or%20'.join([f'country_name=%22{c.title()}%22' for c in arrcountry.split(',')])}")
+            res = await to_thread(requests.get, f"{url}{'%20or%20'.join([f'country_name=%22{c.title()}%22' for c in arrcountry.split(',')])}", headers = headers)
             self.arrcity = [f"{x.get('column_1')}.AIRPORT" for x in res.json().get("results")]
         elif arrcity:
-            res = await to_thread(requests.get, f"{url}{'%20or%20'.join([f'city_name=%22{c}%22%20or%20airport_name%20LIKE%20%22{c.title()}%22' for c in arrcity.split(',')])}")
+            res = await to_thread(requests.get, f"{url}{'%20or%20'.join([f'city_name=%22{c}%22%20or%20airport_name%20LIKE%20%22{c.title()}%22' for c in arrcity.split(',')])}", headers = headers)
             self.arrcity = [f"{x.get('column_1')}.AIRPORT" for x in res.json().get("results")]
         self.vacmin, self.vacmax = vaclength.split('-')
         self.depday, self.depmonth, self.depyear = startperiod.split('-')
@@ -99,9 +114,11 @@ class flightcog(commands.Cog):
         savesearch: bool,
         homecity: str,
         homemaxdistance: int,
-        arrcity: str,
-        arrmaxdistance: int,
+        arrcity: str = None,
+        arrmaxdistance: int = None,
         agechildren: str = "",
+        arrcountry: str = None,
+        arrairports: str = None,
     ):
         """
         search for flights with airports within x km
@@ -121,58 +138,78 @@ class flightcog(commands.Cog):
         arrmaxdistance: look for airports within this range in km
         """
         await inter.send("we do be searching", ephemeral=True, delete_after=15)
+
+        ua = UserAgent()
+        headers = {
+            "User-Agent": ua.random
+        }
         
-        geohome = await to_thread(requests.get, f"https://geocode.maps.co/search?q={homecity}&api_key={self.geoapi}")
+        geohome = await to_thread(requests.get, f"https://geocode.maps.co/search?q={homecity}&api_key={self.geoapi}", headers = headers)
         lathome, lonhome = geohome.json()[0].get("lat"), geohome.json()[0].get("lon")
-        geourlshome = f"https://data.opendatasoft.com/api/explore/v2.1/catalog/datasets/airports-code@public/records?select=column_1&order_by=country_name&limit=100&where=latitude%3C{lathome+(1/111*homemaxdistance)}%20and%20latitude%3E{lathome-(1/111*homemaxdistance)}%20and%20longitude%3C{lonhome+(1/111*homemaxdistance)}%20and%20longitude%3E{lonhome-(1/111*homemaxdistance)}"
-        res = await to_thread(requests.get, geourlshome)
+        geourlshome = f"https://data.opendatasoft.com/api/explore/v2.1/catalog/datasets/airports-code@public/records?select=column_1&order_by=country_name&limit=100&where=latitude%3C{float(lathome)+(1/111*homemaxdistance)}%20and%20latitude%3E{float(lathome)-(1/111*homemaxdistance)}%20and%20longitude%3C{float(lonhome)+(1/111*homemaxdistance)}%20and%20longitude%3E{float(lonhome)-(1/111*homemaxdistance)}"
+        res = await to_thread(requests.get, geourlshome, headers = headers)
         self.depcity = [f"{x.get('column_1')}.AIRPORT" for x in res.json().get("results")]
-        
-        geoarr = await to_thread(requests.get, f"https://geocode.maps.co/search?q={arrcity}&api_key={self.geoapi}")
-        latarr, lonarr = geoarr.json()[0].get("lat"), geoarr.json()[0].get("lon")
-        geourlsarr = f"https://data.opendatasoft.com/api/explore/v2.1/catalog/datasets/airports-code@public/records?select=column_1&order_by=country_name&limit=100&where=latitude%3C{latarr+(1/111*arrmaxdistance)}%20and%20latitude%3E{latarr-(1/111*arrmaxdistance)}%20and%20longitude%3C{lonarr+(1/111*arrmaxdistance)}%20and%20longitude%3E{lonarr-(1/111*arrmaxdistance)}"
-        res = await to_thread(requests.get, geourlsarr)
-        self.arrcity = [f"{x.get('column_1')}.AIRPORT" for x in res.json().get("results")]
+        if arrcity:
+            geoarr = await to_thread(requests.get, f"https://geocode.maps.co/search?q={arrcity}&api_key={self.geoapi}", headers = headers)
+            latarr, lonarr = geoarr.json()[0].get("lat"), geoarr.json()[0].get("lon")
+            geourlsarr = f"https://data.opendatasoft.com/api/explore/v2.1/catalog/datasets/airports-code@public/records?select=column_1&order_by=country_name&limit=100&where=latitude%3C{float(latarr)+(1/111*arrmaxdistance)}%20and%20latitude%3E{float(latarr)-(1/111*arrmaxdistance)}%20and%20longitude%3C{float(lonarr)+(1/111*arrmaxdistance)}%20and%20longitude%3E{float(lonarr)-(1/111*arrmaxdistance)}"
+            res = await to_thread(requests.get, geourlsarr, headers = headers)
+            self.arrcity = [f"{x.get('column_1')}.AIRPORT" for x in res.json().get("results")]
+        elif arrcountry:
+            res = await to_thread(requests.get, f"https://data.opendatasoft.com/api/explore/v2.1/catalog/datasets/airports-code@public/records?select=column_1%2Ccity_name%2Ccountry_name&order_by=country_name&limit=100&where={'%20or%20'.join([f'country_name=%22{c.title()}%22' for c in arrcountry.split(',')])}", headers = headers)
+            self.arrcity = [f"{x.get('column_1')}.AIRPORT" for x in res.json().get("results")]
+        elif arrairports:
+            self.arrcity = [f"{x.upper()}.AIRPORT" for x in arrairports.split(',')]
         self.vacmin, self.vacmax = vaclength.split('-')
         self.depday, self.depmonth, self.depyear = startperiod.split('-')
         self.retday, self.retmonth, self.retyear = endperiod.split('-')
         self.roundtrip = roundtrip
         self.nradults = nradults
-        self.agechildren = '%2C'.join(agechildren.split())
+        self.agechildren = '%2C'.join(agechildren.split(','))
         await inter.send("we do be searching", ephemeral=True, delete_after=15)
         await self.flightscanner()
 
     async def look_for_flights(self, departureTerminals, arrivalTerminals, departdate, returndate) -> None:
+        ua = UserAgent()
+        headers = {
+            "User-Agent": ua.random
+        }
         self.roundtripconf = "ROUNDTRIP"
         if not self.roundtrip:
             self.roundtripconf = "ONEWAY"
-        url = f"https://flights.booking.com/api/flights/?type={self.roundtripconf}&adults={self.nradults}&children={self.agechildren}&cabinClass=ECONOMY&children=&from={'%2C'.join(departureTerminals)}&to={'%2C'.join(arrivalTerminals)}&depart={departdate}&return={returndate}&sort=CHEAPEST&travelPurpose=leisure&duration=33"
+            #    "https://flights.booking.com/api/flights/?type=ROUNDTRIP&adults=5&cabinClass=ECONOMY&children=1%2C3&from=AMS.AIRPORT%2CGNE.AIRPORT&to=LIS.AIRPORT%2CZYF.AIRPORT&depart=2025-05-20&return=2025-06-08&sort=CHEAPEST"
+        url = f"https://flights.booking.com/api/flights/?type={self.roundtripconf}&adults={self.nradults}&cabinClass=ECONOMY&children={self.agechildren}&from={'%2C'.join(departureTerminals)}&to={'%2C'.join(arrivalTerminals)}&depart={departdate}&return={returndate}&sort=CHEAPEST"
         n = 1
         while n:
             if n == 10:
                 return []
-            r = await to_thread(requests.get, url)
+            r = await to_thread(requests.get, url, headers = headers)
             if r.status_code != 200:
                 await sleep(5)
                 n += 1
                 continue
             break
+
         resp = r.json()
         if not resp.get('flightOffers'):
             return []
         flight = [fly for fly in resp.get('flightOffers')]
         retdict = [fl for fl in flight if (fl.get('segments')[0].get('travellerCheckedLuggage') or fl.get('extraProducts'))]
-        realret = [{"dates": [departdate, returndate], "price": int(x.get('priceBreakdown').get('total').get('units')) + int(x.get('extraProducts')[0].get('priceBreakdown').get('total').get('units')) if ( not x.get('segments')[0].get('travellerCheckedLuggage') and x.get('extraProducts') and x.get('extraProducts')[0].get('type') == 'checkedInBaggage' ) else int(x.get('priceBreakdown').get('total').get('units')), "flightTime": x.get('segments')[0].get('totalTime'), "flightTimeback": x.get('segments')[1].get('totalTime'), "depart": x.get('segments')[0].get('departureAirport').get('code'), "arriv": x.get('segments')[0].get('arrivalAirport').get('code'), "departTime": x.get('segments')[0].get('departureTime'), "arrivTime": x.get('segments')[0].get('arrivalTime'), "token": x.get('token')} for x in retdict if int(x.get('priceBreakdown').get('total').get('units')) < 1700 ]
+        realret = [{"dates": [departdate, returndate], "price": int(x.get('priceBreakdown').get('total').get('units')) + int(x.get('extraProducts')[0].get('priceBreakdown').get('total').get('units')) if ( not x.get('segments')[0].get('travellerCheckedLuggage') and x.get('extraProducts') and x.get('extraProducts')[0].get('type') == 'checkedInBaggage' ) else int(x.get('priceBreakdown').get('total').get('units')), "flightTime": x.get('segments')[0].get('totalTime'), "flightTimeback": x.get('segments')[1].get('totalTime'), "depart": x.get('segments')[0].get('departureAirport').get('code'), "arriv": x.get('segments')[0].get('arrivalAirport').get('code'), "departTime": x.get('segments')[0].get('departureTime'), "arrivTime": x.get('segments')[0].get('arrivalTime'), "token": x.get('token')} for x in retdict if int(x.get('priceBreakdown').get('total').get('units')) < 10000 ]
         return realret
         
     async def url_shortener(self, data):
+        ua = UserAgent()
+        headers = {
+            "User-Agent": ua.random
+        }
         api_url = "https://is.gd/create.php"
         param = {'format': 'simple', 'url': f"{baseurl}{data.get('token')}"}
         n = 0
         while n <= 2:
             if n == 2:
                 return f"https://flights.booking.com/flights/X/?type={self.roundtripconf}&adults={self.nradults}&children={self.agechildren}&cabinClass=ECONOMY&from={self.dep}.AIRPORT&to={self.arr}.AIRPORT&depart={data.get('dates')[0][:10]}&return={data.get('dates')[1][:10]}&sort=CHEAPEST"
-            response = await to_thread(requests.get, url=api_url, params=param)
+            response = await to_thread(requests.get, url=api_url, params=param, headers = headers)
             if response.status_code != 200:
                 await sleep(3)
                 n += 1
@@ -192,11 +229,15 @@ class flightcog(commands.Cog):
     async def flightscanner(self):
         dates = await generate_date_range(vacation_range=(datetime(int(self.depyear),int(self.depmonth), int(self.depday)), datetime(int(self.retyear), int(self.retmonth), int(self.retday))), vacation_length=(int(self.vacmin), int(self.vacmax)))
         my_dict = {"data": []}
-        for d in dates:
-            allres = await gather(*[ self.look_for_flights(self.depcity, self.arrcity, d[0], x) for x in d[1:]])
-            for x in allres:
-                my_dict.get("data").extend(x)
-            await sleep(2)
+        departcitiessplit = await split_into_chunks_comp(self.depcity, chunk_size=3)
+        arrcitiessplit = await split_into_chunks_comp(self.arrcity, chunk_size=3)
+        for dprt in departcitiessplit:
+            for ari in arrcitiessplit:
+                for d in dates:
+                    allres = await gather(*[ self.look_for_flights(dprt, ari, d[0], x) for x in d[1:]])
+                    for x in allres:
+                        my_dict.get("data").extend(x)
+                    await sleep(2)
         sorted_data = sorted(my_dict["data"], key=lambda x: x["price"])
         await self.sendflightstodiscord(sorted_data)
 
@@ -205,8 +246,8 @@ class flightcog(commands.Cog):
         sorted_data = sorteddata
         departures = sorted(list(set([data.get('depart') for data in sorted_data])), key=str.lower)
         for dep in departures:
-            lowest_price_under_24 = next((x['price'] for x in sorted_data if 'AMS' in x['depart'] and x['flightTime'] <= 20 * 3600), 1650)
-            lowest_price_above_24 = next((x['price'] for x in sorted_data if 'AMS' in x['depart'] and x['flightTime'] > 20 * 3600), 1610)
+            lowest_price_under_24 = next((x['price'] for x in sorted_data if 'AMS' in x['depart'] and x['flightTime'] <= 20 * 3600), 5000)
+            lowest_price_above_24 = next((x['price'] for x in sorted_data if 'AMS' in x['depart'] and x['flightTime'] > 20 * 3600), 5000)
             if "AMS" in dep:
                 lowest_price_under_24 = 1650
                 lowest_price_above_24 = 1610
