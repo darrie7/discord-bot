5c85544b13de932f72b4321c5e901604d51fb3a4
darrie7 <darnell.r.kluit@gmail.com>
2025-06-11 08:45:06 -0400

    Update jwt.py

--- cogs/jwt.py
+++ cogs/jwt.py
@@ -216,6 +216,7 @@ class Search_Media:
             self.search_term = f"{self.media.get('title')} S{progress_season:02}"
             t_info = await self.media_scraper()
             if not t_info == []:
+                await self.bot.get_channel(793878235066400809).send(f"""```Found for: {self.search_term}```""")
                 mag2del = await self.magnet2deluge(t_info, dl_path)
                 if mag2del:
                     return False
